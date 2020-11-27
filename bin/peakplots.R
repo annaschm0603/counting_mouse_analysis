5ecc2879ee469b0916faa898752aa7b0a9c38665
Elin Axelsson <elin.axelsson@gmi.oeaw.ac.at>
2020-11-27 13:07:57 +0100

    new containers, R can do all plots

--- bin/peakplots.R
+++ bin/peakplots.R
@@ -11,13 +11,13 @@ library(gtools)
 w_col=wes_palette("Moonrise3",5)[c(1,5,3)]
 tot_merged=read.csv("master_anno.csv",sep = ";", stringsAsFactors=F,comment="#")
 psets = grep("narrowPeak",colnames(tot_merged))
-#pdf("peakOV.fig1.pdf")
-#upset(tot_merged,main.bar.color=w_col,sets.bar.color=w_col[1:2])
-#dev.off()
+pdf("peakOV.fig1.pdf")
+upset(tot_merged,main.bar.color=w_col,sets.bar.color=w_col[1:2])
+dev.off()
 
-#CairoPNG("peakOV.fig1.png")
-#upset(tot_merged,main.bar.color=w_col,sets.bar.color=w_col[1:2])
-#dev.off()
+CairoPNG("peakOV.fig1.png")
+upset(tot_merged,main.bar.color=w_col,sets.bar.color=w_col[1:2])
+dev.off()
 
 
 
@@ -54,4 +54,3 @@ par(mfrow=c(1,2))
 boxplot(tot_merged$peakL~tot_merged$comb,col=w_col,names=c(sapply(strsplit(psets,"_"),"[[",1),"both"),main="Master")
 boxplot(lapply(peaks,function(x) y=width(x)),las=1,col=w_col[1:2],names=sapply(strsplit(psets,"_"),"[[",1),main="narrowPeak")
 dev.off()
-
